679e7bab606520e43a1413d198a07fd98e8d397f
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2022-08-08 23:58:11 -0500

    creando el primer componente de testimonio

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+const Testimonial = () => {
+      return(
+        <div className= 'container-testimonial'>
+          <img 
+          className='imagen-testimonial' 
+          src={require('../imagenes/testimonio-emma.png')}
+          alt='imagen de Emma'
+          />
+          <div>
+            <p className='name-testimonial'>Enma Bostian en Suecia</p>
+            <p className='cargo-testimonial'>Ingeniera de Software en Spotify</p>
+            <p className='text-testimonial'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+          </div>
+        </div>
+
+      );
+}
+
+export default Testimonial;
